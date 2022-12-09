6f1b2c924ad4fe0d582d76c463c827186c5f8e5f
alexbjr369 <alexbjr369@gmail.com>
2022-12-09 17:02:34 -0300

    commit

--- src/components/media/media.ts
+++ src/components/media/media.ts
@@ -5,9 +5,21 @@ export const media = (element: HTMLElement) => {
   element.innerHTML = `
     <div class="media">
       <ul class="media__list">
-        <li class="media__item"><i class="fa-brands fa-facebook-f"></i></i></li>
-        <li class="media__item"><i class="fa-brands fa-twitter"></i></li>
-        <li class="media__item"><i class="fa-brands fa-instagram"></i></li>
+        <li>
+          <a class="media__link" href="#">
+            <i class="fa-brands fa-facebook-f"></i>
+          </a>
+        </li>
+        <li>
+          <a class="media__link" href="#">
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a class="media__link" href="#">
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+        </li>
       </ul>
     </div>
   `;
